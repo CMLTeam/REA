68e597fd44fae16e38a797b64cb6bf3d04b6cd15
vgorin <vgrn.at@gmail.com>
2017-05-27 22:07:22 +0300

    add lundb buildings

--- src/test/java/com/cmlteam/model/lun/ExtendLunModel.java
+++ src/test/java/com/cmlteam/model/lun/ExtendLunModel.java
@@ -1,13 +1,16 @@
 package com.cmlteam.model.lun;
 
-import com.cmlteam.util.HttpUtil;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import openchat.util.JsonUtil;
 
+import javax.net.ssl.*;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.security.cert.CertificateException;
 
 /**
  * @author vgorin
@@ -23,14 +26,58 @@ public class ExtendLunModel {
 
 		for(Building building: model.buildings) {
 			long id = building.id;
-			Path p = Paths.get(String.format("~/DEVELOP/cml/REA/lunbuilding_%d.json", id));
-			Files.write(p, fetchBuilding(id).getBytes());
+			Files.write(Paths.get(String.format("building_%d.json", id)), fetchBuilding(id).getBytes());
 			System.out.printf("building %d processed\n", id);
 		}
 	}
 
 	private static String fetchBuilding(long id) throws IOException {
 		String getUrl = String.format("https://novostroyki.lun.ua/map-search/get-building-by-id?id=%d", id);
-		return HttpUtil.fetchPlainText(getUrl);
+		OkHttpClient client = getUnsafeOkHttpClient();
+		Request request = new Request.Builder().url(getUrl).build();
+		Response response = client.newCall(request).execute();
+		return response.body().string();
+	}
+
+	private static OkHttpClient getUnsafeOkHttpClient() {
+		try {
+			// Create a trust manager that does not validate certificate chains
+			final TrustManager[] trustAllCerts = new TrustManager[] {
+					new X509TrustManager() {
+						@Override
+						public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
+						}
+
+						@Override
+						public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
+						}
+
+						@Override
+						public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+							return new java.security.cert.X509Certificate[]{};
+						}
+					}
+			};
+
+			// Install the all-trusting trust manager
+			final SSLContext sslContext = SSLContext.getInstance("SSL");
+			sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+			// Create an ssl socket factory with our all-trusting manager
+			final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+
+			OkHttpClient.Builder builder = new OkHttpClient.Builder();
+			builder.sslSocketFactory(sslSocketFactory, (X509TrustManager)trustAllCerts[0]);
+			builder.hostnameVerifier(new HostnameVerifier() {
+				@Override
+				public boolean verify(String hostname, SSLSession session) {
+					return true;
+				}
+			});
+
+			OkHttpClient okHttpClient = builder.build();
+			return okHttpClient;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
 	}
 }
