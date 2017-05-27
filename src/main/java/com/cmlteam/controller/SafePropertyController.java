14375464427797601aa660b646132601be6db3ab
justrocknrolla <Weblogic1>
2017-05-27 11:50:39 -0400

    fixed imports

--- src/main/java/com/cmlteam/controller/SafePropertyController.java
+++ src/main/java/com/cmlteam/controller/SafePropertyController.java
@@ -1,6 +1,19 @@
 package com.cmlteam.controller;
 
 
+import com.pengrad.telegrambot.BotUtils;
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.TelegramBotAdapter;
+import com.pengrad.telegrambot.model.Chat;
+import com.pengrad.telegrambot.model.Message;
+import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import java.util.Date;
 import java.util.TimeZone;
 import java.util.logging.Logger;
