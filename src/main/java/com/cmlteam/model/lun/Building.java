3d7d516dee5ca7a6a0271e7b8462ef651c9cbc55
vgorin <vgrn.at@gmail.com>
2017-05-27 18:55:55 +0300

    add lun.ua json model

---
+++ src/main/java/com/cmlteam/model/lun/Building.java
@@ -0,0 +1,38 @@
+package com.cmlteam.model.lun;
+
+import openchat.api.messenger.json.AbstractJson;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * @author vgorin
+ *         file created on 5/27/17 6:49 PM
+ */
+
+
+@XmlRootElement
+public class Building extends AbstractJson {
+	@XmlElement
+	public long id;
+	@XmlElement
+	public String state;
+	@XmlElement
+	public Img img;
+	@XmlElement
+	public String synonym;
+	@XmlElement
+	public long regionId;
+	@XmlElement
+	public double lat;
+	@XmlElement
+	public double lng;
+	@XmlElement
+	public City city;
+	@XmlElement
+	public String address;
+	@XmlElement
+	public Point[] polygon;
+	@XmlElement
+	public String analytics;
+}
