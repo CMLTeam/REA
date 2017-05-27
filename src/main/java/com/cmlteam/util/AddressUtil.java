1bc2196a19eef123cb623e1a93bb5222fba7e704
vgorin <vgrn.at@gmail.com>
2017-05-27 18:20:54 +0300

    compilation fix

--- src/main/java/com/cmlteam/util/AddressUtil.java
+++ src/main/java/com/cmlteam/util/AddressUtil.java
@@ -1,5 +1,12 @@
 package com.cmlteam.util;
 
+import com.google.maps.GeoApiContext;
+import com.google.maps.GeocodingApi;
+import com.google.maps.errors.ApiException;
+import com.google.maps.model.GeocodingResult;
+
+import java.io.IOException;
+
 /**
  * @author vgorin
  *         file created on 5/27/17 4:20 PM
@@ -10,7 +17,25 @@ public class AddressUtil {
 	private static final GeoApiContext CTX = new GeoApiContext().setApiKey(PropertyUtil.loadProperty("GEO_API_KEY"));
 
 	public static String resolveAddress(String address) {
+		try {
+			GeocodingResult[] results = GeocodingApi.geocode(CTX, address).await();
+			System.out.println(results);
+			return results[0].toString();
+		}
+		catch(ApiException e) {
+			e.printStackTrace();
+		}
+		catch(InterruptedException e) {
+			e.printStackTrace();
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 
+	public static void main(String[] args) {
+		resolveAddress("ЖК Голосеевский");
 	}
 
 }
