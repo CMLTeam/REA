1c8399230719c0d122ec8c041596d85a90764287
vgorin <vgrn.at@gmail.com>
2017-05-27 18:09:35 +0300

    spring-boot application skeleton 2

---
+++ src/main/java/com/cmlteam/util/AddressUtil.java
@@ -0,0 +1,16 @@
+package com.cmlteam.util;
+
+/**
+ * @author vgorin
+ *         file created on 5/27/17 4:20 PM
+ */
+
+
+public class AddressUtil {
+	private static final GeoApiContext CTX = new GeoApiContext().setApiKey(PropertyUtil.loadProperty("GEO_API_KEY"));
+
+	public static String resolveAddress(String address) {
+
+	}
+
+}
---
+++ src/main/java/com/cmlteam/util/PropertyUtil.java
@@ -0,0 +1,43 @@
+package com.cmlteam.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author vgorin
+ *         file created on 5/27/17 4:32 PM
+ */
+
+
+public class PropertyUtil {
+	private static final Logger log = LoggerFactory.getLogger(PropertyUtil.class);
+
+	public static String loadProperty(String key, String defaultValue) {
+		// try to load property as is
+		String value = loadProperty(key);
+
+		// try to load it lowercase, convert SOME_KEY to some-key
+		if(value == null) {
+			key = key.toLowerCase().replaceAll("_", "-");
+			value = loadProperty(key);
+		}
+
+		// try to load it uppercase, convert some-key to SOME_KEY
+		if(value == null) {
+			key = key.toUpperCase().replaceAll("\\-", "_");
+			value = loadProperty(key);
+		}
+
+		// fallback to default value
+		return value == null? defaultValue: value;
+	}
+
+	public static String loadProperty(String key) {
+		// try to load property from JAVA_OPTS like -Dname=value
+		String value = System.getProperty(key);
+
+		// fallback to system environment variable if value is null
+		return value == null? System.getenv(key): value;
+	}
+
+}
